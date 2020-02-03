2a576952572ea6dd037c1661c7174857eb901a60
unknown <vladislav.puzzyrev@gmail.com>
2020-02-03 22:13:34 +0500

    Updates

---
+++ precache-manifest.6432edb9d57ac095c37307e74230b672.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bd34fcab0ef771faeb652e27337040",
+    "url": "/samurai-network/index.html"
+  },
+  {
+    "revision": "623bf5407f241d1ad96f",
+    "url": "/samurai-network/static/css/3.770de1a8.chunk.css"
+  },
+  {
+    "revision": "3ae76beb5339a7997cbd",
+    "url": "/samurai-network/static/css/4.9f520dc5.chunk.css"
+  },
+  {
+    "revision": "4436ef0f1c8edadcc6cb",
+    "url": "/samurai-network/static/css/main.178538f4.chunk.css"
+  },
+  {
+    "revision": "0d16f1989c57e232efd6",
+    "url": "/samurai-network/static/js/2.0e1e8ee9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/samurai-network/static/js/2.0e1e8ee9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "623bf5407f241d1ad96f",
+    "url": "/samurai-network/static/js/3.02c9e9cc.chunk.js"
+  },
+  {
+    "revision": "3ae76beb5339a7997cbd",
+    "url": "/samurai-network/static/js/4.1667a164.chunk.js"
+  },
+  {
+    "revision": "4436ef0f1c8edadcc6cb",
+    "url": "/samurai-network/static/js/main.0a6f7f83.chunk.js"
+  },
+  {
+    "revision": "c6e9aa49f755410d18ba",
+    "url": "/samurai-network/static/js/runtime-main.e53634fa.js"
+  },
+  {
+    "revision": "b4b850900b327df633f9ef73bac5eaa5",
+    "url": "/samurai-network/static/media/defaultAvatar.b4b85090.png"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/samurai-network/static/media/preloader.584b607f.gif"
+  }
+]);
